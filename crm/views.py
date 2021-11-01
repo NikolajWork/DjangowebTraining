862d5612792c9fa1dacac470b80e78f60ec85d76
NikolajWork <Nikolaj666work@gmail.com>
2021-11-01 18:22:56 +0300

    third_comit

--- crm/views.py
+++ crm/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .models import Order
+from .forms import Orderform
 # Create your views here.
 def first_page(request):
+    form = Orderform()
     object_list = Order.objects.all()
-    return render(request, './index.html', {'object_list': object_list})
+    return render(request, './index.html', {'object_list': object_list, 'form': form})
 
 def thanks_page(request):
     name = request.POST['name']
