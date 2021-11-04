f03ce937858af6ba214f40e8fa42408b80ecce84
NikolajWork <Nikolaj666work@gmail.com>
2021-11-04 19:00:56 +0300

    fourth_comit

--- telebot/botsender.py
+++ telebot/botsender.py
@@ -3,24 +3,41 @@ from .models import TeleSettings
 
 
 def sendTelegram(tg_name, tg_phone):
-    settings = TeleSettings.objects.get(pk=1)
-    token = str(settings.tg_token)
-    text = str(settings.tg_message)
-    chat_id = str(settings.tg_chat)
-    api = 'https://api.telegram.org/bot'
-    method = api + token + '/sendMessage'
+    if TeleSettings.objects.get(pk=1):
+        settings = TeleSettings.objects.get(pk=1)
+        token = str(settings.tg_token)
+        text = str(settings.tg_message)
+        chat_id = str(settings.tg_chat)
+        api = 'https://api.telegram.org/bot'
+        method = api + token + '/sendMessage'
 
 
+        if text.find('{') and text.find('}') and text.rfind('{') and text.rfind('}'):
+            part_1 = text[0:text.find('{')]
+            part_2 = text[text.find('}')+1:text.rfind('{')]
+            part_3 = text[text.rfind('}'):-1]
 
-    part_1 = text[0:text.find('{')]
-    part_2 = text[text.find('}')+1:text.rfind('{')]
-    part_3 = text[text.rfind('}'):-1]
+            text_slice = part_1 + tg_name + part_2 + tg_phone + part_3
 
-    text_slice = part_1 + tg_name +part_2 + tg_phone +part_3
+        else:
+            text_slice = text
+        try:
+            req = requests.post(method, data={
+                'chat_id': chat_id,
+                'text': text_slice
+            })
+        except:
+            pass
 
-    req = requests.post(method, data={
-        'chat_id': chat_id,
-        'text': text_slice
-    })
+        finally:
+            if req.status_code != 200:
+                print('Ошибка отправки')
+            elif req.status_code == 500:
+                print('Ошибка сервера')
+            else:
+                print('Всё ок!')
+
+    else:
+        pass
 
 
